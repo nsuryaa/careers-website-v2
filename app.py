cf91f991a506018c10a462ec6c9d17042be7e79c
nsuryaa <suryasedulous@gmail.com>
2024-02-01 18:09:22 +0530

    created dynamic pages to display each job role

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 
 #Creating object of class
 app = Flask(__name__)
@@ -35,12 +35,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     jobs = load_jobs_from_db()
-    return render_template('home.html',jobs=jobs,company_name = 'myStartup')
+    return render_template('home.html',jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    # return jsonify(job)
+    if not job:
+        return "Not Found",404
+    return render_template('jobpage.html',job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -13,3 +13,14 @@ def load_jobs_from_db():
         for row in result.fetchall():
             jobs.append(dict(zip(result.keys(), row)))
         return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs WHERE id = :id").bindparams(id=id))
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            job_dict = dict(zip(result.keys(), rows[0]))
+
+            return job_dict
