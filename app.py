feb7606480ab49aed7df44b6094b035efcd5ea87
nsuryaa <suryasedulous@gmail.com>
2024-02-01 12:50:32 +0530

    added connection to database

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
 from flask import Flask,render_template,jsonify
+from database import load_jobs_from_db
 
 #Creating object of class
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru,India',
-        'salary': 'Rs. 1,00,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Delhi,India',
-        'salary': 'Rs. 15,00,000'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote',
-    },
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'San Francisco, USA',
-        'salary': '$150,000'
-    }
-]
+# JOBS = [
+#     {
+#         'id': 1,
+#         'title': 'Data Analyst',
+#         'location': 'Bengaluru,India',
+#         'salary': 'Rs. 1,00,000'
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Data Scientist',
+#         'location': 'Delhi,India',
+#         'salary': 'Rs. 15,00,000'
+#     },
+#     {
+#         'id': 3,
+#         'title': 'Frontend Engineer',
+#         'location': 'Remote',
+#     },
+#     {
+#         'id': 4,
+#         'title': 'Backend Engineer',
+#         'location': 'San Francisco, USA',
+#         'salary': '$150,000'
+#     }
+# ]
+
 
 #Creating a route
 @app.route("/")
 def hello_world():
-    return render_template('home.html',jobs=JOBS,company_name = 'myStartup')
+    jobs = load_jobs_from_db()
+    return render_template('home.html',jobs=jobs,company_name = 'myStartup')
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 
 if __name__ == "__main__":
